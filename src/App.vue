37f01d740dce2a9da45b650f0165a49a623d2437
VeronicaVega00 <mvardilavega5@gmail.com>
2025-02-13 22:42:18 -0500

    This is the First commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+
+
+<template>
+  <div>
+    <HeroSection />
+
+    <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  </div>
+</template>
+<script setup>
+import HeroSection from "@/components/HeroSection.vue";
+</script>
+
---
+++ src/components/HeroSection.vue
@@ -0,0 +1,4 @@
+<template>
+  <h1>this is th hero section</h1>
+
+</template>
